b36eeec815217f42835e86adb1ead2f7be3eb721
jeremy-liss <jeremyliss@gmail.com>
2022-02-01 11:24:47 +1300

    Initial commit

---
+++ CoD.py
@@ -0,0 +1,184 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5 import uic
+from pathlib import Path
+import sys
+
+import pp_file_counter
+
+class WorkerSignals(QObject):
+	finished = pyqtSignal()
+	logger = pyqtSignal(object)
+	progress = pyqtSignal(object)
+
+class Worker(QThread):
+	def __init__(self, parent):
+		super(Worker, self).__init__()
+
+		self.signals = WorkerSignals()
+		self.finished = self.signals.finished
+		self.progress = self.signals.progress
+		self.logger = self.signals.logger
+		self.file_counter = None
+
+	def setParams(self, directory, title_code, start_date, end_date, folders, file_types):
+		self.directory = directory
+		self.title_code = title_code
+		self.start_date = start_date
+		self.end_date = end_date
+		self.file_types = file_types
+		self.folders = folders
+
+	def stop(self):
+		self.file_counter.stop()
+
+	@pyqtSlot()
+	def run(self):
+		if self.file_counter:
+			self.file_counter = None
+
+		self.file_counter = pp_file_counter.FileCounter()
+		self.file_counter.count_files(self.directory, self.title_code, self.start_date, self.end_date, self.folders, self.file_types, self.logger, self.progress)
+		self.finished.emit()
+
+
+class CoDUI(QMainWindow):
+	def __init__(self):
+		super(CoDUI, self).__init__()
+		
+		uic.loadUi("Cod.ui", self)
+
+		self.counting = False
+		self.worker = None
+		self.folders = set()
+		self.file_types = set()
+
+		self.directory_input = self.findChild(QLineEdit, "directory_input")
+		self.directory_button = self.findChild(QPushButton, "directory_button")
+		self.directory_button.setIcon(self.style().standardIcon(QStyle.SP_DirOpenIcon))
+		self.title_code_input = self.findChild(QLineEdit, "title_code_input")
+		self.start_date_input = self.findChild(QLineEdit, "start_date_input")
+		self.end_date_input = self.findChild(QLineEdit, "end_date_input")
+		self.start_button = self.findChild(QPushButton, "start_button")
+		self.results_box = self.findChild(QTextBrowser, "results_box")
+		self.progress_text = self.findChild(QTextBrowser, "progress_text")
+
+		self.pm_group = self.findChild(QGroupBox, "pm_group")
+
+		self.ie_mets_group = self.findChild(QGroupBox, "ie_mets_group")
+
+		self.mm_group = self.findChild(QGroupBox, "mm_group")
+		self.mm_alto_check = self.findChild(QCheckBox, "mm_alto_check")
+		self.mm_mets_check = self.findChild(QCheckBox, "mm_mets_check")
+		self.mm_tiff_check = self.findChild(QCheckBox, "mm_tiff_check")
+
+		self.ac_group = self.findChild(QGroupBox, "ac_group")
+		self.ac_page_check = self.findChild(QCheckBox, "ac_page_check")
+		self.ac_issue_check = self.findChild(QCheckBox, "ac_issue_check")
+
+		date_validator = QIntValidator(0, 99999999, self)
+		self.start_date_input.setValidator(date_validator)
+		self.end_date_input.setValidator(date_validator)
+
+		self.directory_button.clicked.connect(self.choose_directory)
+		self.start_button.clicked.connect(self.count_files)
+
+		self.show()
+
+	def add_file_types(self):
+		self.folders = set()
+		self.file_types = set()
+
+		if self.pm_group.isChecked():
+			self.folders.add(self.pm_group.title())
+
+		if self.ie_mets_group.isChecked():
+			self.folders.add(self.ie_mets_group.title())
+
+		if self.mm_group.isChecked():
+			self.folders.add(self.mm_group.title())
+			if self.mm_alto_check.isChecked():
+				self.file_types.add(self.mm_alto_check.text())
+			if self.mm_mets_check.isChecked():
+				self.file_types.add(self.mm_mets_check.text())
+			if self.mm_tiff_check.isChecked():
+				self.file_types.add(self.mm_tiff_check.text())
+
+		if self.ac_group.isChecked():
+			self.folders.add(self.ac_group.title())
+			if self.ac_page_check.isChecked():
+				self.file_types.add(self.ac_page_check.text())
+			if self.ac_issue_check.isChecked():
+				self.file_types.add(self.ac_issue_check.text())
+
+	def choose_directory(self):
+		# prod:
+		# dialog = QFileDialog(self, "Select directory", str(Path.home()))
+		# testing:
+		dialog = QFileDialog(self, "Select directory", "/media/sf_Y_DRIVE/paperspast/objects/14/other/Newspapers/OCR")
+
+		dialog.setFileMode(QFileDialog.DirectoryOnly)
+		dialog.exec_()
+		selected_directory = None
+		if len(dialog.selectedFiles()) == 1:
+			selected_directory = dialog.selectedFiles()[0]
+
+		if selected_directory:
+			self.directory_input.setText(selected_directory)
+
+	def progress_handler(self, value):
+		self.progress_text.setText(value)
+
+	def logger_handler(self, value):
+		self.results_box.append(value)
+
+	def finishWorker(self):
+		self.start_button.setText('Start')
+		self.worker.quit()
+		self.counting = False
+
+	def count_files(self):
+		if self.counting:
+			self.start_button.setText('Start')
+			self.counting = False
+			if self.worker:
+				self.worker.stop()
+
+		else:
+			directory = self.directory_input.text().strip()
+			title_code = self.title_code_input.text().strip().upper()
+			start_date = self.start_date_input.text().strip()
+			end_date = self.end_date_input.text().strip()
+
+			self.add_file_types()
+
+			self.results_box.clear()
+
+			if directory == '' or title_code == '' or start_date == '' or end_date == '':
+				self.progress_text.setText('Please enter all fields')
+
+			elif start_date > end_date:
+				self.progress_text.setText('Make sure the start date is earlier than the end date')
+
+			elif 'PM_01' not in self.folders and 'IE_METS' not in self.folders and len(self.file_types) == 0:
+				self.progress_text.setText('Please select a file type')
+
+			else:
+				self.worker = Worker(self)
+				self.worker.setParams(directory, title_code, start_date, end_date, self.folders, self.file_types)
+				self.worker.logger.connect(self.logger_handler)
+				self.worker.progress.connect(self.progress_handler)
+				self.worker.finished.connect(self.finishWorker)
+
+				self.start_button.setText('Cancel')
+				self.results_box.clear()
+				self.progress_text.setText('Starting')
+				self.counting = True
+
+				self.worker.start()
+
+
+app = QApplication(sys.argv)
+UIWindow = CoDUI()
+app.exec_()
---
+++ pp_file_counter.py
@@ -0,0 +1,145 @@
+import math, re, sys, time
+from pathlib import Path
+
+class FileCounter():
+	def __init__(self):
+		self.terminate = False
+
+	def stop(self):
+		self.terminate = True
+
+	def convert_size(self, size_bytes):
+	   if size_bytes == 0:
+	       return "0B"
+	   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+	   i = int(math.floor(math.log(size_bytes, 1024)))
+	   p = math.pow(1024, i)
+	   s = round(size_bytes / p, 2)
+	   return "%s %s" % (s, size_name[i])
+
+	def log_handler(self, message):
+		if self.logger:
+			self.logger.emit(message)
+		elif self.logger == None:
+			print(message)
+
+	def progress_handler(self, message):
+		if self.progress:
+			self.progress.emit(message)
+		elif self.progress == None:
+			print(message)
+
+	def count_files(self, directory, titlecode, start_date, end_date, folders, file_types, logger=None, progress=None):
+		self.logger = logger
+		self.progress = progress
+
+		issue_name_pattern = re.compile('[A-Z]{2,5}[_][\d]{8}')
+
+		DIR = Path(directory)
+		if not DIR.is_dir():
+			self.progress_handler("Directory dosen't exist")
+			return
+
+		titlecode = titlecode.upper()
+
+		if DIR.stem != titlecode:	
+			path_with_titlecode = DIR / titlecode
+			if not path_with_titlecode.is_dir():
+				self.progress_handler("Title code not found in that directory")
+				return
+		else:
+			path_with_titlecode = DIR
+
+		start_year = int(start_date[0:4])
+		end_year = int(end_date[0:4])
+
+		total = 0;
+		size = 0;
+
+		self.log_handler("Directory: " + directory)
+		# self.log_handler("File type: " + file_type)
+		self.log_handler("Titlecode: " + titlecode)
+		self.log_handler("Start date: " + start_date)
+		self.log_handler("End date: " + end_date + "\n")
+
+		start_time = time.time()
+		for year in path_with_titlecode.glob("*"):
+			if int(year.name) >= start_year and int(year.name) <= end_year:
+				self.progress_handler("Counting files for " + year.name)
+				for issue in year.glob("*"):
+					# Pull out the date from the folder name
+					date = int(re.sub(r'^.*?_', '', issue.name))
+					if (issue_name_pattern.match(issue.name)) and (date >= int(start_date) and date <= int(end_date)):
+						for folder in issue.glob("*"):
+							if folder.name in folders:
+								for item in folder.iterdir():
+									if self.terminate:
+										self.progress_handler("Process cancelled")
+										self.terminate = False
+										return
+									if item.is_file:
+										if folder.name == 'IE_METS' and item.name == (issue.name + '_IE_METS.xml'):
+											total += 1
+											size += item.stat().st_size
+										if folder.name == 'PM_01' and (item.suffix == '.tif' or item.suffix == '.tiff'):
+											total += 1
+											size += item.stat().st_size
+										if folder.name == 'MM_01':
+											if (item.suffix == '.tif' or item.suffix == '.tiff') and 'TIFF' in file_types:
+												total += 1
+												size += item.stat().st_size
+											if item.name == 'mets.xml' and 'METS' in file_types:
+												total += 1
+												size += item.stat().st_size
+											if item.suffix == '.xml' and item.name != 'mets.xml' and 'ALTO' in file_types:
+												total += 1
+												size += item.stat().st_size
+										if folder.name == 'AC_01':
+											if item.name == issue.name + '.pdf' and 'Issue PDF' in file_types:
+												total += 1
+												size += item.stat().st_size
+											if item.name != issue.name + '.pdf' and item.suffix == '.pdf' and 'Page PDF' in file_types:
+												total += 1
+												size += item.stat().st_size 
+
+										if progress:
+											progress.emit(str(total))
+										else:
+											print(".", sep='', end='', flush=True)
+				if logger == None:							
+					print("\n")
+
+		end_time = time.time()
+		self.progress_handler("Finished in %.2f seconds" % (end_time - start_time))
+
+		if self.logger == None:	
+			print("Directory: " + directory)
+			# print("File type: " + file_type)
+			print("Titlecode: " + titlecode)
+			print("Start date: " + start_date)
+			print("End date: " + end_date + "\n")
+
+		# if file_type == 'issues':
+		# 	self.log_handler("Number of matching issues: " + str(total) + "\n")
+		# else:
+		self.log_handler("Number of matching files: " + str(total))
+		self.log_handler("Total size of files: " + self.convert_size(size) + "\n")
+
+		return [total, self.convert_size(size)]
+
+if __name__ == '__main__':
+	commands = {'directory': '', 'titlecode': '', 'start_date': '', 'end_date': '', 'file_type': ''}
+	if len(sys.argv) < len(commands):
+		print("Please enter all fields")
+		exit()
+		
+	for i in range(len(sys.argv)-1):
+		try:
+			params = sys.argv[i+1].split('=', 1)
+			commands[params[0]] = params[1]
+		except Exception as e:
+			print(e)
+
+	file_counter = FileCounter()
+	file_counter.count_files(commands['directory'], commands['titlecode'], commands['start_date'], commands['end_date'], commands['file_type'])
+	
