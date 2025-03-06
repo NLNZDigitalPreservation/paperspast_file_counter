f3497a2cf764a39f22a441b61c5c05900a5d49bf
jeremy-liss <jeremy.liss@dia.govt.nz>
2025-03-07 11:05:13 +1300

    Adjust box sizes

--- CoD.py
+++ CoD.py
@@ -87,15 +87,19 @@ class CoDUI(QMainWindow):
 		self.files_radio.adjustSize() 
 
 		self.pm_group = self.findChild(QGroupBox, "pm_group")
+		self.pm_group.adjustSize()
 
 		self.ie_mets_group = self.findChild(QGroupBox, "ie_mets_group")
+		self.ie_mets_group.adjustSize()
 
 		self.mm_group = self.findChild(QGroupBox, "mm_group")
+		self.mm_group.adjustSize()
 		self.mm_alto_check = self.findChild(QCheckBox, "mm_alto_check")
 		self.mm_mets_check = self.findChild(QCheckBox, "mm_mets_check")
 		self.mm_tiff_check = self.findChild(QCheckBox, "mm_tiff_check")
 
 		self.ac_group = self.findChild(QGroupBox, "ac_group")
+		self.ac_group.adjustSize()
 		self.ac_page_check = self.findChild(QCheckBox, "ac_page_check")
 		self.ac_issue_check = self.findChild(QCheckBox, "ac_issue_check")
 
